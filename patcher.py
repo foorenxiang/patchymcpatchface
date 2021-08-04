f1d333ac11fbf0a12b91a6c3db3f91f28148f31e
Ren Xiang <foorenxiang@gmail.com>
2021-08-05 03:18:54 +0800

    added placeholder for patch function

--- patcher.py
+++ patcher.py
@@ -1,9 +1,11 @@
+import sys
+import mypackage.foo
+from patch_package.baz import patch_function
+
+mypackage
+
+
 def patch():
-    import sys
-    import mypackage.foo
-    from patch_package.baz import patch_function
-
-    mypackage
-
-    # patching from the parent module is sufficient
+    sys.modules["mypackage"].foo.target_function = "abracadabra"
+    sys.modules["mypackage"].foo.target_function = 42
     sys.modules["mypackage"].foo.target_function = patch_function
