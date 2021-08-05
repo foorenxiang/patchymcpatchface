6c18591c6a4d6ce0503014209400d6e81d3db599
Ren Xiang <foorenxiang@gmail.com>
2021-08-06 01:30:40 +0800

    simplified patch_apply

--- patch_apply.py
+++ patch_apply.py
@@ -20,29 +20,16 @@ def patch_apply(target_object: str, patch_object):
 
     object_heritage_iter = iter(object_heritage)
 
-    def check_target_object_exists(
-        obj=sys.modules[next(object_heritage_iter)], attr=next(object_heritage_iter)
-    ):
-        try:
-            check_target_object_exists(getattr(obj, attr), next(object_heritage_iter))
-        except StopIteration:
-            assert hasattr(
-                obj, attr
-            ), f"{target_object} does not exist in {target_module}!"
-
-    check_target_object_exists()
-
-    object_heritage_iter = iter(object_heritage)
-
     def assign_patch(
         obj=sys.modules[next(object_heritage_iter)],
         attr: str = next(object_heritage_iter),
     ):
         try:
-            next_attr = next(object_heritage_iter)
+            assign_patch(getattr(obj, attr), next(object_heritage_iter))
         except StopIteration:
-            return setattr(obj, attr, patch_object)
-
-        assign_patch(getattr(obj, attr), next_attr)
+            assert hasattr(
+                obj, attr
+            ), f"{target_object} does not exist in {target_module}!"
+            setattr(obj, attr, patch_object)
 
     assign_patch()
