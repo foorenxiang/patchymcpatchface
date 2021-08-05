3868040b97952bd71c2038955dbeac4f57dd7877
Ren Xiang <foorenxiang@gmail.com>
2021-08-05 09:55:46 +0800

    applied attribute checking to ensure patch will pass

--- patch_package/baz.py
+++ patch_package/baz.py
@@ -6,4 +6,11 @@ def patch_function():
 
 
 def patch():
+    assert hasattr(sys.modules["mypackage"].foo, "target_function")
+    # assert hasattr(
+    #     sys.modules["mypackage"].non_existent_module, "target_function"
+    # )  # this will throw attribute error
+    # assert hasattr(
+    #     sys.modules["mypackage"].foo, "non_existent_target_function"
+    # )  # this will throw attribute error
     sys.modules["mypackage"].foo.target_function = patch_function
