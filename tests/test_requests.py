c504ff4f89116b62fba78f62c7c17b4907537d3f
Ren Xiang <foorenxiang@gmail.com>
2021-08-08 03:38:12 +0800

    added new test for version > 0.1.13

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -6,7 +6,7 @@ sys.path.append(os.getcwd())
 import patchymcpatchface as pf
 
 
-def test_requests():
+def test_requests_target_ancestor():
     def request_inner():
         print("I am the patched request!")
         return {
@@ -27,5 +27,23 @@ def test_requests():
     assert isinstance(response, dict)
 
 
-if __name__ == "__main__":
-    test_requests()
+def test_requests_target_object():
+    def request_inner():
+        print("I am the patched request!")
+        return {
+            "title": "I",
+            "body": "am",
+            "userId": 42,
+        }
+
+    from package_to_be_patched.request_call import requests_function, Session
+
+    pf.patch_apply(
+        (Session, "request"),
+        lambda *args, **kwargs: request_inner(),
+    )
+
+    response = requests_function()
+    print(response)
+
+    assert isinstance(response, dict)
